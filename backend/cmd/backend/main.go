62c37f7759394deef1212da48cd4e296c36a3451
Looper Tech <loopertechtools@gmail.com>
2022-11-20 12:49:54 -0500

    Cleanup Main

--- backend/cmd/backend/main.go
+++ backend/cmd/backend/main.go
@@ -1,28 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"looper-sets-backend/pkg/config"
 	"looper-sets-backend/pkg/routes"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	log.Println("Starting backend...")
 
-	config.LoadEnvironmentVariables()
+	// Load env vars
+	fmt.Println("Loading environment variables...")
+	envErr := godotenv.Load(".env")
+	if envErr != nil {
+		log.Fatal("Error loading .env file...")
+	}
+	fmt.Println(os.Getenv("APP_ENV"))
 
+	// Setup server
 	host := "localhost:"
 	if os.Getenv("APP_ENV") != "dev" {
 		gin.SetMode(gin.ReleaseMode)
 		host = ":"
 	}
-
 	server := gin.Default()
+
+	// Add routes
 	routes.AddPingPongRoutes(server)
  
+	// Start server
 	log.Println("Starting server...")
 	server.Run(host + "8080")
 }
